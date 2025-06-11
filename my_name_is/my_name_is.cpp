f1044d2c45d0586ff9ce22666df87aada103ebd2
zhansa001 <52686075+zhansa001@users.noreply.github.com>
2025-06-11 13:38:41 -0400

    Add files via upload

---
+++ my_name_is/my_name_is.cpp
@@ -0,0 +1,13 @@
+//Sandy Zhang
+//my_name_is
+//CS 23001
+//Due Date: August 30, 2024
+
+#include <iostream>
+using std::cin; using std::cout; using std::endl;
+
+int main() {
+cout << "My name is Sandy Zhang." << endl;
+
+return 0;
+}
